3196445ea2818e51002ed09d6f90062920879c47
ThulinaWickramasinghe <thulinawickramasinghe@gmail.com>
2023-05-10 15:36:42 +0530

    Add basic code template

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using System.Windows.Input;
+using ReactiveUI;
+
+namespace MusicStore.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    public MainWindowViewModel()
+    {
+        BuyMusicCommand = ReactiveCommand.Create(() =>
+        {
+            // Code here will be executed when the button is clicked.
+        });
+    }
+
+    public ICommand BuyMusicCommand { get; }
+}
