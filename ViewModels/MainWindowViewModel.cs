ec15dedb4ed19a85dafe6a1e9d15f3e8ccfb31fa
ThulinaWickramasinghe <thulinawickramasinghe@gmail.com>
2023-05-12 02:36:43 +0530

    Add data persistence

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,26 @@
-﻿using System.Reactive.Linq;
+﻿using System.Collections.ObjectModel;
+using System.Reactive.Linq;
 using System.Windows.Input;
 using ReactiveUI;
+using System.Reactive.Concurrency;
+using System.Linq;
+using System;
+using MusicStore.Models;
 
 namespace MusicStore.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
+    private bool _collectionEmpty;
+
+    public bool CollectionEmpty
+    {
+        get => _collectionEmpty;
+        set => this.RaiseAndSetIfChanged(ref _collectionEmpty, value);
+    }
+
+    public ObservableCollection<AlbumViewModel> Albums { get; } = new();
+
     public MainWindowViewModel()
     {
         ShowDialog = new Interaction<MusicStoreViewModel, AlbumViewModel>();
@@ -14,9 +29,36 @@ public class MainWindowViewModel : ViewModelBase
         {
             var store = new MusicStoreViewModel();
             var result = await ShowDialog.Handle(store);
+
+            if (result != null)
+            {
+                Albums.Add(result);
+
+                await result.SaveToDiskAsync();
+            }
         });
+
+        this.WhenAnyValue(x => x.Albums.Count)
+            .Subscribe(x => CollectionEmpty = x == 0);
+
+        RxApp.MainThreadScheduler.Schedule(LoadAlbums);
     }
 
     public ICommand BuyMusicCommand { get; }
     public Interaction<MusicStoreViewModel, AlbumViewModel> ShowDialog { get; }
+
+    private async void LoadAlbums()
+    {
+        var albums = (await Album.LoadCachedAsync()).Select(x => new AlbumViewModel(x));
+
+        foreach (var album in albums)
+        {
+            Albums.Add(album);
+        }
+
+        foreach (var album in Albums.ToList())
+        {
+            await album.LoadCover();
+        }
+    }
 }
